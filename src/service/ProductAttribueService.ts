0b7999d06dac9fec05d16bd19d783e20ddf00120
tuankhanh205 <khanhtqtph49625@gmail.com>
2025-05-07 00:39:51 +0700

    feat: add/update/ productAtribute

--- src/service/ProductAttribueService.ts
+++ src/service/ProductAttribueService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import type { ProductAttributeResponse } from '../model/productAttribute';
+import type { ProductAttributeRequest } from '../model/productAttribute';
 import type { ApiResponse } from "../utils/ApiResponse";
 const API_URL = "http://localhost:8080/api/v1/admin/attribute";
 const axiosInstance = axios.create();
@@ -24,7 +25,33 @@ export const ProductAttributeService = {
             console.error(`Error fetching product attribute with ID ${id}:`, error);
             throw new Error('Failed to fetch product attribute. Please try again later.');
         }
-    }
+    },
+    //addadd
+    saveProductAttribute: async (data: ProductAttributeRequest): Promise<ApiResponse<ProductAttributeResponse>> => {
+        try {
+            const response = await axiosInstance.post<ApiResponse<ProductAttributeResponse>>(`${API_URL}/save`, data);
+            return response.data;
+        } catch (error) {
+            console.error("Lỗi khi lưu thuộc tính sản phẩm:", error);
+            throw new Error("Không thể lưu thuộc tính. Vui lòng thử lại sau.");
+        }
+    },
+    updateProductAttribute: async (
+        id: number,
+        data: ProductAttributeRequest
+      ): Promise<ApiResponse<ProductAttributeResponse>> => {
+        try {
+          const response = await axiosInstance.put<ApiResponse<ProductAttributeResponse>>(
+            `${API_URL}/update/${id}`,
+            data
+          );
+          return response.data;
+        } catch (error) {
+          console.error("Lỗi khi cập nhật thuộc tính sản phẩm:", error);
+          throw new Error("Không thể cập nhật thuộc tính. Vui lòng thử lại sau.");
+        }
+      }      
+   
 }
 
 
